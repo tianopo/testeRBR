218178c51974a978d4cd18393f59285ee3e9ff8e
tianopo <matheuslink18@hotmail.com>
2024-05-17 17:50:39 -0300

    retirando parte dos erros dfesnecessários

--- backend/utils/handleControllerResponse.ts
+++ backend/utils/handleControllerResponse.ts
@@ -1,9 +1,5 @@
 /* c8 ignore start */
 
-import { AuthenticationError } from './errors/Authentication/AuthenticationError'
-import { InsuficientPermissionsError } from './errors/Authentication/InsuficientPermissionsError'
-import { InvalidCredentialsError } from './errors/Authentication/InvalidCredentialsError'
-import { InvalidTokenError } from './errors/Authentication/InvalidTokenError'
 import { CustomError } from './errors/CustomError'
 import { GenericError } from './errors/GenericError'
 import { RouteNotFoundError } from './errors/RouteNotFoundError'
@@ -11,18 +7,6 @@ import { RouteNotFoundError } from './errors/RouteNotFoundError'
 export function handleControllerResponse(response: unknown | Error): IResponseType {
   if (response instanceof Error) {
     switch (true) {
-      case response instanceof InvalidTokenError:
-        return { status: 400, responseController: { error: response.message } }
-
-      case response instanceof AuthenticationError:
-        return { status: 401, responseController: { error: response.message } }
-
-      case response instanceof InsuficientPermissionsError:
-        return { status: 403, responseController: { error: response.message } }
-
-      case response instanceof InvalidCredentialsError:
-        return { status: 403, responseController: { error: response.message } }
-
       case response instanceof RouteNotFoundError:
         return { status: 404, responseController: { error: response.message } }
 
