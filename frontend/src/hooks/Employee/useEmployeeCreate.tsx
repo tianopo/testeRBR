2e188daf7bcc2399aa794060291c04defde9a3b8
tianopo <matheuslink18@hotmail.com>
2024-05-17 23:20:53 -0300

    hook de criar pronto

---
+++ frontend/src/hooks/Employee/useEmployeeCreate.tsx
@@ -0,0 +1,42 @@
+import { IEmployeeDTO } from '@/@types/ModelsAtributos'
+import { api } from '@/config/api'
+import { apiRoutes } from '@/config/apiRoutes'
+import Yup from '@/utils/Yup'
+import { responseError, responseSuccess } from '@/utils/responseHandler'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useMutation } from '@tanstack/react-query'
+import { AxiosError } from 'axios'
+import { useForm } from 'react-hook-form'
+import { useRouter } from 'next/router';
+
+export const useEmployeeCreate = () => {
+  const router = useRouter()
+
+  const { mutate, isLoading, status } = useMutation(endPoint, {
+    onSuccess: () => {
+      router.back();
+      responseSuccess("Funcionário criado com sucesso")
+    },
+    onError: (erro: AxiosError) => responseError(erro),
+  })
+
+  const schema = Yup.object().shape({
+    nome: Yup.string().required().label("nome"),
+    cargo: Yup.string().required().label("cargo"),
+    departamento: Yup.string().required().label("departamento"),
+  })
+
+  type Schema = Yup.InferType<typeof schema>
+
+  const context = useForm({
+    resolver: yupResolver(schema),
+    reValidateMode: 'onChange',
+  })
+
+  async function endPoint(data: Schema): Promise<IEmployeeDTO> {
+    const result = await api().post(apiRoutes.employeeCreate, data)
+    return result.data.data
+  }
+
+  return { mutate, isLoading, context, status }
+}
